c8c0dd2637d7c258771bedcba87c0fd1eba6e67f
Ilya Lyamin <lyamin.ilya.s@gmail.com>
2023-08-25 19:32:27 +0400

    Change function name

--- script/script.js
+++ script/script.js
@@ -1,15 +1,14 @@
 const cards = document.getElementsByClassName("quality__card");
-console.log(cards);
 
-const inverseClass = function (el) {
+const toggleActiveCard = function (el) {
   if (el.classList.contains("quality__card_active")) el.classList.remove("quality__card_active");
   else el.classList.add("quality__card_active");
 };
 
 for (let i = 0; i < cards.length; i++) {
   let timerId = setTimeout(function tick() {
-    inverseClass(cards[i]);
-    setTimeout(() => inverseClass(cards[i]), 2000);
+    toggleActiveCard(cards[i]);
+    setTimeout(() => toggleActiveCard(cards[i]), 2000);
     timerId = setTimeout(tick, 2000 * cards.length);
   }, i*2000 );
 }
